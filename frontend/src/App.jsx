1704080ded30a18684933f6968fe3ce47063ba50
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-09-21 12:10:04 +0500

    dashboard creating

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
     <>
-      <h1 className="text-red-800">Hello World!</h1>
+      <Dashboard />
     </>
   );
 }
---
+++ frontend/src/pages/dashboard/index.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div className="bg-[#2F4252] h-screen w-[17%]"></div>;
+}
+
+export default Dashboard;
