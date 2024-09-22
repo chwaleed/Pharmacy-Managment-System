867b378ee14df535c0f030b19ecfd32d2937b790
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-09-22 20:59:46 +0500

    Database connection added and some pakages also added express and mongoose and dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,14 @@
-console.log("HELLO World!");
-console.log("HELLO LOVE");
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+const databaseUrl = process.env.DATABASE_URL;
+
+// Database Connection
+mongoose
+  .connect(databaseUrl)
+  .then(() => console.log("Database Connected Successfuly"))
+  .catch((err) => {
+    console.log(err.message);
+    process.exit();
+  });
