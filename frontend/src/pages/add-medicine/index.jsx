cb4e304309c09b51fcc04ac0f4c2f42da949a57a
chwaleed <150054046+chwaleed@users.noreply.github.com>
2024-10-05 21:55:31 +0500

    data table added

--- frontend/src/pages/add-medicine/index.jsx
+++ frontend/src/pages/add-medicine/index.jsx
@@ -11,6 +11,62 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Edit } from "lucide-react";
+import { X } from "lucide-react";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
 
 function AddMedicine() {
   const form = useForm();
@@ -38,6 +94,42 @@ function AddMedicine() {
         <span className="flex justify-center items-center gap-2">
           Search: <Input className="text-[1.1rem]" type="text" />
         </span>
+      </div>
+      <div>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Product Name</TableHead>
+              <TableHead>Batch No</TableHead>
+              <TableHead>Supplier</TableHead>
+              <TableHead>Company</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {invoices.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell>Bloach Enterprise</TableCell>
+                <TableCell className="">$250.00</TableCell>
+                <TableCell>40</TableCell>
+                <TableCell className="flex gap-4">
+                  <span>
+                    <Edit />
+                  </span>
+                  <span>
+                    <X />
+                  </span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
